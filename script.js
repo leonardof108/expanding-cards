621c90f9eb971d31af548c7c988fc4ade4cd7f84
Leonardo Ferreira <118021845+leonardof108@users.noreply.github.com>
2024-10-13 11:36:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    panels.forEach(function(panel){
+        panel.classList.remove("active");    
+    })
+    panel.classList.add("active");
+  });
+});
